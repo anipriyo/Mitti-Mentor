38e072da181c814bef6c35f9dd1ee7479c978e50
Anipriyo Mandal <50947191+anipriyo@users.noreply.github.com>
2024-04-20 14:59:30 +0000

    Testing Upgradea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #from flask_ngrok import run_with_ngrok
 import os  #type: ignore
-import numpy as np #type: ignore
+import numpy as np  #type: ignore
 from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model  #type: ignore
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
@@ -25,13 +25,14 @@ Crops: cotton, wheat, rice, pulses, millets, tobacco, oil seeds, potatoes and fr
 
 
 def predict_model(img_path, model):
-    img = load_img(img_path, target_size=(224, 224))
-    img = img_to_array(img)
-    img = img / 255
-    img = np.expand_dims(img, axis = 0)
+  img = load_img(img_path, target_size=(224, 224))
+  img = img_to_array(img)
+  img = img / 255
+  img = np.expand_dims(img, axis=0)
 
-    result = np.argmax(model.predict(img))
-    return result
+  result = np.argmax(model.predict(img))
+  return result
+
 
 @app.route("/predictSoil", methods=["GET", "POST"])
 def predictSoil():
@@ -40,81 +41,100 @@ def predictSoil():
     nameUploadedImg = uploadedImg.filename
     print("You have uploaded ", nameUploadedImg)
     print("Predicting soil.....")
-    if nameUploadedImg is None :
+    if nameUploadedImg is None:
       return "An error occurred"
-    uploadedImg_path = os.path.join('static','uploads', secure_filename(nameUploadedImg))
+    uploadedImg_path = os.path.join('static', 'uploads',
+                                    secure_filename(nameUploadedImg))
     uploadedImg.save(uploadedImg_path)
     print("Predicting soil.....")
     SoilNet = load_model(model_path)
     result = predict_model(uploadedImg_path, SoilNet)
-    print("Soil Detected : ",result)
+    print("Soil Detected : ", result)
     if (result == 0):
-      return render_template("afterUploadSoil.html", soil_type='Alluvial', user_image_path=uploadedImg_path, desc=alluvial)
+      return render_template("afterUploadSoil.html",
+                             soil_type='Alluvial',
+                             user_image_path=uploadedImg_path,
+                             desc=alluvial)
     elif (result == 1):
-      return render_template("afterUploadSoil.html", soil_type='Black', user_image_path=uploadedImg_path, desc=black)
+      return render_template("afterUploadSoil.html",
+                             soil_type='Black',
+                             user_image_path=uploadedImg_path,
+                             desc=black)
     elif result == 2:
-      return render_template("afterUploadSoil.html", soil_type='Clay', user_image_path=uploadedImg_path, desc=clay)
+      return render_template("afterUploadSoil.html",
+                             soil_type='Clay',
+                             user_image_path=uploadedImg_path,
+                             desc=clay)
     elif result == 3:
-      return render_template("afterUploadSoil.html", soil_type='Red', user_image_path=uploadedImg_path, desc=red)
+      return render_template("afterUploadSoil.html",
+                             soil_type='Red',
+                             user_image_path=uploadedImg_path,
+                             desc=red)
     else:
       return "An error occurred"
   return render_template("uploadSoil.html")
 
+
 @app.route("/predictCrop", methods=["GET", "POST"])
 def predictCrop():
-    if request.method == "POST":
-        uploadedImg = request.files["image"]
-        nameUploadedImg = uploadedImg.filename
-        print("You have uploaded ", nameUploadedImg)
-        if nameUploadedImg is None :
-          return "An error occurred"
-        uploadedImg_path = os.path.join('static','uploads', nameUploadedImg)
-        uploadedImg.save(uploadedImg_path)
+  if request.method == "POST":
+    uploadedImg = request.files["image"]
+    nameUploadedImg = uploadedImg.filename
+    print("You have uploaded ", nameUploadedImg)
+    if nameUploadedImg is None:
+      return "An error occurred"
+    uploadedImg_path = os.path.join('static', 'uploads', nameUploadedImg)
+    uploadedImg.save(uploadedImg_path)
 
-        print("Predicting crop.....")
-        SoilNet = load_model(model_path)
-        result = predict_model(uploadedImg_path, SoilNet)
+    print("Predicting crop.....")
+    SoilNet = load_model(model_path)
+    result = predict_model(uploadedImg_path, SoilNet)
 
-        if (result == 0):
-            return render_template("alluvialCrop.html")
-        elif (result == 1):
-            return render_template("blackCrop.html")
-        elif result == 2:
-            return render_template("clayCrop.html")
-        elif result == 3:
-            return render_template("redCrop.html")
-        return "An error occurred"
-    return render_template("uploadCrop.html")
-
+    if (result == 0):
+      return render_template("alluvialCrop.html")
+    elif (result == 1):
+      return render_template("blackCrop.html")
+    elif result == 2:
+      return render_template("clayCrop.html")
+    elif result == 3:
+      return render_template("redCrop.html")
+    return "An error occurred"
+  return render_template("uploadCrop.html")
 
 
 @app.route('/home')
 def home():
   return render_template('index.html')
-  
+
+
 @app.route("/uploadSoil")
 def uploadSoil():
-    return render_template("uploadSoil.html")
+  return render_template("uploadSoil.html")
+
 
 @app.route("/uploadCrop")
 def uploadCrop():
-    return render_template("uploadCrop.html")
+  return render_template("uploadCrop.html")
+
 
 @app.route("/alluvial")
 def Alluvial():
-    return render_template("alluvialCrop.html")
+  return render_template("alluvialCrop.html")
+
 
 @app.route("/black")
 def Black():
-    return render_template("blackCrop.html")
+  return render_template("blackCrop.html")
+
 
 @app.route("/red")
 def Red():
-    return render_template("redCrop.html")
+  return render_template("redCrop.html")
+
 
 @app.route("/clay")
 def Clay():
-    return render_template("clayCrop.html")
+  return render_template("clayCrop.html")
 
 
 app.run(host='0.0.0.0', port=3000)
